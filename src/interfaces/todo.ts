aee98a250f75ab45a34b15962d2b7026b280aa9f
maksim.gorban <maksim.gorban@glowbyteconsulting.com>
2024-10-23 11:22:43 +0300

    feature: Added todo create controlled form

--- src/interfaces/todo.ts
+++ src/interfaces/todo.ts
@@ -2,3 +2,8 @@ export interface ICreateTodo {
     title: string
     description: string
 }
+
+export interface ICreateTodoFormErrors {
+    title: string | null
+    description: string | null
+}
--- src/pages/Todo/TodoCreateForm.tsx
+++ src/pages/Todo/TodoCreateForm.tsx
@@ -2,32 +2,42 @@ import React, { useState } from 'react'
 
 import axios from 'axios'
 
-import { ICreateTodo } from '../../interfaces/todo'
+import { ICreateTodo, ICreateTodoFormErrors } from '../../interfaces/todo'
+
 
 export default function TodoCreateForm() {
 
     const [formData, setFormData] = useState<ICreateTodo>({title: "", description: ""})
+    const [formErrors, setFormErrors] = useState<ICreateTodoFormErrors>({title: null, description: null})
 
-    const handleChange = (event) => {
-        const {name, value} = event.target
+    function handleChange(event){
+        const { name, value } = event.target
+
         setFormData((prevFormData) => ({...prevFormData, [name]: value}))
+
+            if (value.length >= 5) {
+                setFormErrors({...formErrors, [name]: null})
+            } else {
+                setFormErrors({...formErrors, [name]: "Less than 5 characters"})
+            }
     }
 
-    const handleSubmit = (event) => {
+    function onSubmit(event){
         event.preventDefault()
-        axios.post("http://localhost:8000/todo/", formData)
-        setFormData({title: "", description: ""})
+        
     }
 
     return (
-        <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="title">Title:</label>
-            <input type="text" name="title" id="title" onChange={handleChange}/>
+        <form action="" onSubmit={onSubmit}>
+            <label htmlFor="title">Title: </label>
+            <input type="text" name="title" id="title" value={formData.title} onChange={handleChange}/><br/>
+            {formErrors.title && <div style = {{color: 'red'}}>{formErrors.title}</div>}
 
-            <label htmlFor="description">Description</label>
-            <input type="text" name="description" id="description" onChange={handleChange}/>
+            <label htmlFor="description">Description: </label>
+            <input type="text" name="description" id="description" value={formData.description} onChange={handleChange}/><br/>
+            {formErrors.description && <div style = {{color: 'red'}}>{formErrors.description}</div>}
 
-            <button type="submit">Create</button>
+            <button type="submit">Submit</button>
         </form>
     )
 }
